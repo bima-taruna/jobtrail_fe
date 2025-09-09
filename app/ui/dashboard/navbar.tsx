42f7d4be8f9b5397afc00e4e89f36d3c3eb8ea99
bima-taruna <bimataruna21@gmail.com>
2025-09-09 21:44:12 +0700

    create navbar

---
+++ app/ui/dashboard/navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuItem,
+  NavigationMenuContent,
+  navigationMenuTriggerStyle,
+  NavigationMenuLink,
+} from "@/app/ui/components/navigation-menu";
+import Link from "next/link";
+import { poppins } from "@/app/ui/fonts";
+import { ThemeButton } from "./theme-button";
+
+export function Navbar() {
+  return (
+    <NavigationMenu className="fixed top-0 w-full max-w-none p-3  justify-between">
+      <NavigationMenuItem className={`list-none`}>
+        <NavigationMenuLink
+          asChild
+          className={`${navigationMenuTriggerStyle()} ${
+            poppins.className
+          } font-bold text-2xl list-none`}
+        >
+          <Link href="/">JobTrail</Link>
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+
+      {/* Right side */}
+      <NavigationMenuList className="w-full">
+        <NavigationMenuItem>
+          <ThemeButton />
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+            <Link href="/login">Login</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
