a97311d16874f1853f1bece2f904f2b51e1225c0
bima-taruna <bimataruna21@gmail.com>
2025-09-09 21:45:00 +0700

    add fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Poppins, Inter } from "next/font/google";
+
+export const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const inter = Inter({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
