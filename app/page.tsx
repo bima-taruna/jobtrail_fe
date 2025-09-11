9e1dc16e5932673dd9c546bf6d98e7dd18701631
bima-taruna <bimataruna21@gmail.com>
2025-09-11 12:36:22 +0700

    some fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { poppins } from "@/app/ui/fonts";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <h1 className={`${poppins.className} text-5xl text-center font-bold`}>
           JobTrail
