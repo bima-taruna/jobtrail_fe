ebd1e676c61ad1e24d3e57e7b8135599e23a22bd
bima-taruna <bimataruna21@gmail.com>
2025-09-18 21:57:14 +0700

    redirect to dashboard if session is valid

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,13 @@
 import { poppins } from "@/app/ui/fonts";
 import LoginForm from "../ui/auth/LoginForm";
+import { auth } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  if (session) {
+    redirect("/dashboard");
+  }
   return (
     <div className="items-center justify-items-center min-h-screen p-20 pb-20 gap-16 md:p-8">
       <div className="mt-20 md:mt-0 flex flex-col gap-[32px] row-start-2 items-center">
