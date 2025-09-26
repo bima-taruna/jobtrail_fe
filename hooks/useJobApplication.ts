9c0bea536128cc229b242f73e6ff07e8dec6f498
bima-taruna <bimataruna21@gmail.com>
2025-09-26 20:41:00 +0700

    add create job application function to add data to backend

--- hooks/useJobApplication.ts
+++ hooks/useJobApplication.ts
@@ -1,27 +1,48 @@
 import {
+  JobApplicationInput,
   JobApplicationResponse,
   JobStatus,
 } from "@/app/definitions/job_application";
-import { getUsersJobApplication } from "@/lib/api/jobApplication";
-import { useQuery } from "@tanstack/react-query";
+import {
+  createJobApplication,
+  getUsersJobApplication,
+} from "@/lib/api/jobApplication";
+import {
+  useMutation,
+  useQuery,
+  useSuspenseQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 
-export function useJobApplication(
+export function useGetJobApplication(
   userId: string,
   page: number = 1,
   pageSize: number = 10,
   search?: string,
   status?: JobStatus
 ) {
-  return useQuery<JobApplicationResponse>({
+  return useSuspenseQuery<JobApplicationResponse>({
     queryKey: ["jobApplications", userId, page, pageSize, search, status],
     queryFn: () =>
-      getUsersJobApplication(
-        userId,
-        page,
-        pageSize,
-        search ?? "",
-        status ?? ("" as JobStatus)
-      ),
-    enabled: !!userId,
+      userId
+        ? getUsersJobApplication(
+            userId,
+            page,
+            pageSize,
+            search ?? "",
+            status ?? ("" as JobStatus)
+          )
+        : Promise.resolve({ data: [], page: 1, page_size: 0, total: 0 }),
+  });
+}
+
+export function useCreateJobApplication() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: JobApplicationInput) => createJobApplication(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["jobApplications"] });
+    },
   });
 }
