9d33ccd6bda7a0a7ac3eb49a8c064baa23e9dc0e
bima-taruna <bimataruna21@gmail.com>
2025-09-26 20:39:25 +0700

    add suspense and add job application button

--- app/job_application/page.tsx
+++ app/job_application/page.tsx
@@ -1,6 +1,8 @@
 import { poppins } from "@/app/ui/fonts";
-import JobApplicationTable from "../ui/job_application/job-application-table";
-import { auth } from "../api/auth/[...nextauth]/route";
+import JobApplicationTable from "@/app/ui/job_application/job-application-table";
+import { auth } from "@/app/api/auth/[...nextauth]/route";
+import { AddDialog } from "@/app/ui/job_application/add-dialog";
+import { Suspense } from "react";
 
 export default async function Page() {
   const session = await auth();
@@ -10,11 +12,18 @@ export default async function Page() {
       <h1 className={`${poppins.className} text-2xl font-bold`}>
         List of Job Application
       </h1>
-      {userId ? (
-        <JobApplicationTable userId={userId} />
-      ) : (
-        <p>No user logged in</p>
-      )}
+      <section className="mt-3 flex-col">
+        <div className="justify-self-end">
+          <AddDialog />
+        </div>
+        {userId ? (
+          <Suspense fallback={<p>Loading....</p>}>
+            <JobApplicationTable userId={userId} />
+          </Suspense>
+        ) : (
+          <p>No user logged in</p>
+        )}
+      </section>
     </div>
   );
 }
