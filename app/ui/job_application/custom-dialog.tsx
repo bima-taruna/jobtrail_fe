b4248e045aadd158623577b53056dd77fe3c46e0
bima-taruna <bimataruna21@gmail.com>
2025-09-29 15:41:06 +0700

    refact : rename add dialog to custom dialog to make it reusable

--- app/ui/job_application/custom-dialog.tsx
+++ app/ui/job_application/custom-dialog.tsx
@@ -13,23 +13,31 @@ import { PlusIcon } from "lucide-react";
 import { Label } from "@/app/ui/components/label";
 import { Input } from "@/app/ui/components/input";
 import { CreateJobApplicationForm } from "@/app/ui/job_application/add-form";
+import React from "react";
 
-export function AddDialog() {
+interface DialogProps {
+  dialog_title: string;
+  dialog_action?: string;
+  dialog_description: string;
+  children: React.ReactNode;
+  triggerButton: React.ReactNode;
+}
+
+export function CustomDialog({
+  dialog_title,
+  triggerButton,
+  dialog_description,
+  children,
+}: DialogProps) {
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Button>
-          <PlusIcon /> Add Application
-        </Button>
-      </DialogTrigger>
+      <DialogTrigger asChild>{triggerButton}</DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Add Job Application</DialogTitle>
-          <DialogDescription>
-            Fill form the data of job application that you are currently hiring
-          </DialogDescription>
+          <DialogTitle>{dialog_title}</DialogTitle>
+          <DialogDescription>{dialog_description}</DialogDescription>
         </DialogHeader>
-        <CreateJobApplicationForm />
+        {children}
       </DialogContent>
     </Dialog>
   );
