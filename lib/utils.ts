e3eeeff4e0d4956da77dacd5e94469bf92213ea8
bima-taruna <bimataruna21@gmail.com>
2025-10-06 09:58:13 +0700

    create format date util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(params: string) {
+  const date = new Date(params);
+  const formattedDate = date.toLocaleDateString("id-ID");
+  return formattedDate;
 }
