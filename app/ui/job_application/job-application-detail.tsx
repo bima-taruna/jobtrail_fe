197488a74896a86da135f3372f16b1284dcaf103
bima-taruna <bimataruna21@gmail.com>
2025-10-15 11:19:33 +0700

    create jobdetail component

--- app/ui/job_application/job-application-detail.tsx
+++ app/ui/job_application/job-application-detail.tsx
@@ -9,6 +9,11 @@ import {
 } from "@/app/ui/components/card";
 import { poppins } from "../fonts";
 import { formatDate } from "@/lib/utils";
+import JobDetailTimeline from "@/app/ui/job_application/job-detail-timeline";
+import { CustomDialog } from "./custom-dialog";
+import { Button } from "../components/button";
+import { PlusIcon } from "lucide-react";
+import { CreateJobApplicationForm } from "./add-form";
 
 interface JobDetailProps {
   id: string;
@@ -22,7 +27,7 @@ export function JobApplicationDetail({ id }: JobDetailProps) {
   }
   return (
     <>
-      <section>
+      <section className="overflow-x-hidden">
         <h1 className={`${poppins.className} font-bold text-3xl`}>
           {data?.job_title}
         </h1>
@@ -33,7 +38,9 @@ export function JobApplicationDetail({ id }: JobDetailProps) {
       <section className="mt-5">
         <Card>
           <CardHeader>
-            <CardTitle>Job Information</CardTitle>
+            <CardTitle className={`${poppins.className} text-xl font-bold`}>
+              Job Information
+            </CardTitle>
           </CardHeader>
           <CardContent>
             <div className="flex gap-2">
@@ -41,8 +48,45 @@ export function JobApplicationDetail({ id }: JobDetailProps) {
               <p>{formatDate(data ? data.application_date.toString() : "")}</p>
             </div>
             <div className="flex gap-2">
-              <p className="font-bold">Status : </p> <p>{data?.status}</p>
+              <p className="font-bold">Status : </p>{" "}
+              <p>{data?.current_status}</p>
             </div>
+          </CardContent>
+        </Card>
+      </section>
+      <section className="mt-5 grid grid-cols-1 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle
+              className={`${poppins.className} text-xl font-bold flex justify-between`}
+            >
+              Job Timelines
+              <CustomDialog
+                dialog_title="Add Job Application"
+                dialog_action="Add Job Application"
+                dialog_description="Fill the information of job application that you're currently hiring"
+                triggerButton={
+                  <Button>
+                    <PlusIcon /> Add Job Application
+                  </Button>
+                }
+              >
+                <CreateJobApplicationForm />
+              </CustomDialog>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <JobDetailTimeline timelines={data.timelines} />
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle className={`${poppins.className} text-xl font-bold`}>
+              Job Interviews
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <JobDetailTimeline timelines={data.timelines} />
           </CardContent>
         </Card>
       </section>
